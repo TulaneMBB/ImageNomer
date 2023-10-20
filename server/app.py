180bf3aed2eeee80b6bcf9e4aa4d009393ed9d07
Anton Orlichenko <aorliche@gmail.com>
2023-10-19 23:56:56 -0500

    Working on filter subjects and pagination

--- server/app.py
+++ server/app.py
@@ -22,14 +22,14 @@ groups = [dict(name='All', sel=False)]
 
 # Subjects and subject pagination
 n_subs_per_page = 8
-subs = []
-filtered_subs = []
+subs = list(range(1, 100))
+filtered_subs = subs
 subs_page = 1
 
 # For server-sent events
 cv = threading.Condition()
 
-to_update = dict(GroupsList=True)
+to_update = dict(GroupsList=True, SubsPagination=True, SubsList=True)
 
 # Home screen
 @app.route('/')
@@ -44,12 +44,24 @@ def get_component(comp):
             html = render_template('groups-list.html', groups=enumerate(groups))
             return html
         if comp == 'SubsPagination':
-            last_page = (len(subs)-1) // n_subs_per_page + 1
-            pages = list(range(1, last_page + 1))
-            st = subs_page-2 if subs_page > 2 else 0
-            end = subs_page+2
-            view_pages = pages[st:end]
-            html = render_template('subs-pagination.html', view_pages=view_pages)
+            if len(filtered_subs) == 0:
+                view_pages = []
+                has_prev = False
+                has_next = False
+            else:
+                last_page = (len(filtered_subs)-1) // n_subs_per_page + 1
+                pages = list(range(1, last_page + 1))
+                st = subs_page-3 if subs_page > 2 else 0
+                end = subs_page+2
+                view_pages = pages[st:end]
+                has_prev = subs_page > 1
+                has_next = subs_page < last_page
+            html = render_template('subs-pagination.html', view_pages=view_pages, has_prev=has_prev, has_next=has_next)
+            return html
+        if comp == 'SubsList':
+            st = (subs_page-1) * n_subs_per_page
+            end = st + n_subs_per_page
+            html = render_template('subs-list.html', subs=filtered_subs[st:end])
             return html
 
 # Server-sent events
@@ -68,6 +80,7 @@ def sse():
                 cv.wait()
     return Response(event_stream(), mimetype='text/event-stream')
 
+# Create a group
 @app.route('/create-group', methods=['POST'])
 def create_group():
     args = request.form
@@ -77,6 +90,25 @@ def create_group():
         return jsonify(err)
     groups.append(dict(name=args['group-text'], sel=False))
     to_update['GroupsList'] = True
+    with cv:
+        cv.notify()
+    return ('', 204)
+
+# Filter subjects
+@app.route('/filter-subjects', methods=['POST'])
+def filter_subjects():
+    args = request.form
+    err = validate_args(['filter-text'], args, '/filter-subjects')
+    if err is not None:
+        print(err)
+        return jsonify(err)
+    global filtered_subs
+    pattern = args['filter-text']
+    filtered_subs = [s for s in subs if pattern in str(s)]
+    if len(filtered_subs) < (subs_page-1) * n_subs_per_page + 1:
+        subs_page = 1
+    to_update['SubsPagination'] = True
+    to_update['SubsList'] = True
     with cv:
         cv.notify()
     return ('', 204)
